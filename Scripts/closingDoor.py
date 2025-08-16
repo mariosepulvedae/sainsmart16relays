4e6cb708a83748f80e67acd27aa1829a379cfe30
Mario Sepúlveda <42502242+mariosepulvedae@users.noreply.github.com>
2025-08-15 23:41:56 -0600

    Create closingDoor.py

---
+++ Scripts/closingDoor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+def closingD(reles):
+    reles[0]=False
+    reles[1]=True
+    reles[2]=False
+    reles[3]=False
+    reles[4]=False
+
+
+    reles[5]=True
+    reles[6]=False
+
+    reles[7]=False 
+    reles[8]=True
+
+    reles[9]=True
+    reles[10]=True
+    reles[11]=False
+    reles[12]=True
+    
+    return reles
